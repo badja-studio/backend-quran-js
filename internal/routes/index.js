fed3187e1444c776bf9b9fc45417d6bbbbfc7165
putra samb <putra@samb.co.id>
2025-11-24 14:17:31 +0700

    feat: Add authentication routes and update Swagger documentation with an authentication tag and bearer security scheme.

--- internal/routes/index.js
+++ internal/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const surahRoutes = require('./surah.routes');
 const healthRoutes = require('./health.routes');
+const authRoutes = require('./auth.routes');
 
 const router = express.Router();
 
@@ -8,6 +9,7 @@ const router = express.Router();
 router.use('/', healthRoutes);
 
 // API v1 routes
+router.use('/v1/auth', authRoutes);
 router.use('/v1', surahRoutes);
 
 module.exports = router;
